79970c3034ef7bda665772b60f830d6693ecf6f6
ahamilto99 <ajhamilton15@hotmail.com>
2022-01-31 07:59:33 -0500

    Completed lesson 3

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,15 +2,34 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+    <div class="pagination">
+      <router-link
+        id="prev-page"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+        >&#60; Prev Page</router-link
+      >
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+        >Next Page &#62;</router-link
+      >
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard
   },
@@ -20,13 +39,22 @@ export default {
     }
   },
   created() {
-    EventService.getEvents()
-      .then(response => {
+    watchEffect(async () => {
+      this.events = null
+      try {
+        const response = await EventService.getEvents(2, this.page) // 2 events on the "current" pg
         this.events = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+        this.totalEvents = response.headers['x-total-count']
+      } catch (e) {
+        console.log(e)
+      }
+    })
+  },
+  computed: {
+    hasNextPage() {
+      const totalPages = Math.ceil(this.totalEvents / 2)
+      return this.page < totalPages
+    }
   }
 }
 </script>
@@ -36,5 +64,24 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-pref {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
