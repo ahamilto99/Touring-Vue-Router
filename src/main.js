7735c1f90be43ce96cc06e496d71520a0600e24d
ahamilto99 <ajhamilton15@hotmail.com>
2022-02-02 20:08:10 -0500

    Added flash message for successful registration

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// reactive global store object
+const GStore = reactive({ flashMessage: '' })
+
 createApp(App)
   .use(store)
   .use(router)
+  .provide('GStore', GStore) // provide so other objects can inject it
   .mount('#app')
